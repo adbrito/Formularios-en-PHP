c9d5a20d99b9c52f87faebd7c872cb41e67fbd17
Allison Brito Mendoza <adbrito@espol.edu.ec>
2020-12-13 18:19:38 -0500

    agregando icono a header

--- mascotas.php
+++ mascotas.php
@@ -6,15 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
 </head>
 
 <body>
+<div>
+        <h1 class="text-center">Mascotas registradas
+        <i style='font-size:24px' class='fas'>&#xf1b0;</i>
+        </h1>
+        
+
+    </div>
     <table class="table">
         <thead>
             <tr>
                 <th scope="col">#</th>
                 <th scope="col">Dueño</th>
-                <th scope="col">Telefono</th>
+                <th scope="col">Teléfono</th>
                 <th scope="col">Dirección</th>
                 <th scope="col">Nombre de la mascota</th>
                 <th scope="col">Edad</th>
